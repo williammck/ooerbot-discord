2ed14febea664a50de4c987e664a6b8d3a2080fc
William McKinnerney <git@williammck.net>
2018-03-27 23:19:16 -0500

    Move event binding into the Module class itself

--- src/modules/Module.js
+++ src/modules/Module.js
@@ -1,6 +1,19 @@
 class Module {
   constructor (client) {
     this.client = client
+    this.bindEvents()
+  }
+
+  bindEvents () {
+    const proto = Object.getPrototypeOf(this)
+    let properties = Object.getOwnPropertyNames(proto).filter(p => p.startsWith('on'))
+
+    for (let property of properties) {
+      let event = property.substring(2)
+      event = event.charAt(0).toLowerCase() + event.substr(1);
+
+      this.client.on(event, this[property].bind(this))
+    }
   }
 }
 
