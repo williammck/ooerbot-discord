55bb2dfb71ef73ff9ee8368a5d605d801aed67a4
William McKinnerney <git@williammck.net>
2018-03-29 10:57:15 -0500

    Activate new modules, temporarily deactivate PinModule

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,19 @@
 const ActivityModule = require('./ActivityModule')
 const PinModule = require('./PinModule')
+const DadModule = require('./DadModule')
+const MentionModule = require('./MentionModule')
+const AyyModule = require('./AyyModule')
+const ZalgoModule = require('./ZalgoModule')
 
 exports.initialize = function initialize (client) {
-  const modules = [ActivityModule, PinModule]
+  const modules = [
+    ActivityModule,
+    //PinModule,
+    DadModule,
+    MentionModule,
+    AyyModule,
+    ZalgoModule
+  ]
 
   for (let module of modules) {
     new module(client)
