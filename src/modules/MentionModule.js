37ea750df442e93e3d31bec0b82e724dfffc5b45
William McKinnerney <git@williammck.net>
2018-04-07 14:17:01 -0500

    Add double pings

--- src/modules/MentionModule.js
+++ src/modules/MentionModule.js
@@ -7,6 +7,21 @@ class MentionModule extends Module {
     if (message.mentions.users.get(this.client.user.id)) {
       message.channel.send(message.author.toString())
     }
+
+    if (!message.mentions.everyone) {
+      let mentionMsgTxt = '';
+      if (message.mentions.users.size) {
+        for (let user of message.mentions.users.array()) {
+          mentionMsgTxt += user.toString() + ' '
+        }
+      }
+      if (message.mentions.roles.size) {
+        for (let role of message.mentions.roles.array()) {
+          mentionMsgTxt += role.toString() + ' '
+        }
+      }
+      if (mentionMsgTxt.length) message.channel.send(mentionMsgTxt)
+    }
   }
 }
 
